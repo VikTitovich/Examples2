bcdd6d14635458e69b57bdddd26c232d878ad427
Viktor Titovich <raffityinet@gmail.com>
2023-02-07 14:49:48 +0300

    Add code max from two numbers

--- program1/Program.cs
+++ program1/Program.cs
@@ -1,2 +1,36 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Task_1();
+
+void Task_1()
+{
+    int firstNumber = ReadInt ("First Number");
+    int secondNumber = ReadInt ("Second Number");
+
+    PrintMore (firstNumber, secondNumber);
+
+}
+
+
+
+int ReadInt (string argument)
+{
+    Console.WriteLine($"Input {argument}");
+    int number;
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Error! Try again.");
+    }
+return number;
+}
+
+void PrintMore (int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine($"{firstNumber} > {secondNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"{secondNumber} > {firstNumber}");
+    }
+}
