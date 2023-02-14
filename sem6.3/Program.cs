3bb46298d9eb8e5f6f9e280f3fa968bd76df3792
Viktor Titovich <raffityinet@gmail.com>
2023-02-14 11:58:43 +0300

    TransformNumber

---
+++ sem6.3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int a = ReadInt("число");
+
+Console.WriteLine(TransformNumber(a));
+
+int ReadInt(string arg)
+{
+    int i;
+    Console.Write($"Введите {arg}: ");
+
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        Console.Write("Неверное значение. Повторите: ");
+    }
+
+    return i;
+}
+
+string TransformNumber(int a)
+{
+    string result = String.Empty;
+    while (a > 0)
+    {
+        result = a % 2 + result;
+
+        a /= 2;
+    }
+    return result;
+}
