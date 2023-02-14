3b48cf1ac3bd6cb89fa36ee6b68fbf93a9227cc3
Viktor Titovich <raffityinet@gmail.com>
2023-02-14 11:48:28 +0300

    Triangle

---
+++ srem6.2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход 3 числа
+//и проверяет, может ли существовать треугольник со
+//сторонами такой длины
+
+int a = ReadInt("сторону треугольника");
+int b = ReadInt("сторону треугольника");
+int c = ReadInt("сторону треугольника");
+
+CheckTriangle(a, b, c);
+
+int ReadInt(string arg)
+{
+    int i;
+    Console.Write($"Введите {arg}: ");
+
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        Console.Write("Неверное значение. Повторите: ");
+    }
+
+    return i;
+}
+
+void CheckTriangle(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine($"Треугольник со сторонами: {a}, {b} и {c} может существовать");
+    }
+    else
+    {
+        Console.WriteLine($"Треугольник со сторонами: {a}, {b} и {c} не может существовать");
+    }
+}
+
