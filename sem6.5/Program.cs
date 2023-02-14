8de49a7d336ffe85a02859c729d3ac370238d517
Viktor Titovich <raffityinet@gmail.com>
2023-02-14 15:42:42 +0300

    PrintArray

---
+++ sem6.5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая будет создавать 
+// копию заданного массива с помощью поэлементного копирования
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+
+    return copyArray;
+}
+
+void PrintNmbers(int[] array)
+{
+    Console.WriteLine(string.Join(", ", array));
+}
+
+int[] array = GetRandomArray(10, 0, 100);
+
+PrintNmbers(array);
+
+int[] newArr = CopyArray(array);
+
+PrintNmbers(newArr);
+
